8a110642bffb9f4d731a21523c2e53572c222037
blah4 <blah4@tutanota.com>
2019-03-11 18:51:41 +0100

    What to do?

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import './App.scss';
 
-import AddFormButton from './components/AddFormButton';
-import AddForm from './components/AddForm';
-import ShowOrdersButton from './components/ShowOrdersButton';
-import Orders from './components/Orders';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <AddFormButton/>
-        <ShowOrdersButton/>
-        <AddForm/>
-        <Orders/>
+
       </div>      
     );
   }
